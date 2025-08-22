c661e108b19c23b05d9559bc3c0f1a6d13eaeb09
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-08-22 15:38:42 -0400

    Began implementing emulator
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

---
+++ reproducible-builds/docker/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Hack to avoid the AVD being put in ${ANDROID_AVD_HOME}/.. for some reason
+REAL_AVD_DIR=${ANDROID_AVD_HOME}/${AVD_NAME}
+
+# Running the Android Emulator on a new virtual device
+avdmanager --silent create avd \
+    -n "${AVD_NAME}" \
+    -k "${AVD_PACKAGE}" \
+    -p "${REAL_AVD_DIR}" \
+    $@
+emulator -avd "${AVD_NAME}" &
+
+# Installing APK(s)
+adb install -t "${APK}"
+
+# Forwarding ports
+# https://developer.android.com/tools/adb#forwardports
+adb forward tcp:8080 tcp:8080
+adb forward udp:8080 udp:8080
+adb forward tcp:8081 local:logd
+
+# Blocking main process while emulator runs in background
+tail -f '/dev/null'
