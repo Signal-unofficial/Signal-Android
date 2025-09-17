adb14a62ffd2119cdc9a31e962765dae40a17758
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-09-17 04:50:08 -0400

    Updated keystore file path
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- gen-keystore.ps1
+++ gen-keystore.ps1
@@ -1,4 +1,4 @@
 $APK_ALIAS="Signal"
-$KEYSTORE_FILE="dev.keystore"
+$KEYSTORE_FILE="./reproducible-builds/secrets/dev.keystore"
 
 keytool -genkey -v -keystore "${KEYSTORE_FILE}" -alias "${APK_ALIAS}" -keyalg RSA -keysize 4096
--- gen-keystore.sh
+++ gen-keystore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 APK_ALIAS='Signal'
-KEYSTORE_FILE='dev.keystore'
+KEYSTORE_FILE='./reproducible-builds/secrets/dev.keystore'
 
 keytool -genkey -v -keystore "${KEYSTORE_FILE}" -alias "${APK_ALIAS}" -keyalg RSA -keysize 4096
